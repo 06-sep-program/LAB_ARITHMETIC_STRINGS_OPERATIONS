1633debaccdd4ded745e22d575aa08af043453e9
Joud Almusa <joud.almousa1@outlook.com>
2026-09-09 14:24:40 +0300

    Complete Arithemtic and Strings operations lab

---
+++ bonus.py
@@ -0,0 +1,14 @@
+print("bonus: ")
+
+sentence = "Python is a useful language and Python is fun to learn"
+word = "Python"
+
+print()
+print("Length of sentence:", len(sentence))
+print("Index of first occurrence:", sentence.find(word))
+print("Number of occurrences:", sentence.count(word))
+print("Uppercase:", sentence.upper())
+print("Lowercase:", sentence.lower())
+print("Replaced sentence:", sentence.replace(word, "Java"))
+print("Last character:", sentence[-1])
+print()
