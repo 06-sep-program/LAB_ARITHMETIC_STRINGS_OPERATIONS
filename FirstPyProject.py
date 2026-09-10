83704392c63f68d54ac236afb00719907ac92c9b
Yousef <Yousefaalmayouf@gmail.com>
2026-09-10 14:23:06 +0300

    new version

--- FirstPyProject.py
+++ FirstPyProject.py
@@ -1,5 +1,15 @@
 #Grocery store prograrm
 price = 2.99
 quantity = 3
-tax_rate = 7.5
-print("Price of the item"+ price)
+tax_rate_For_Print = 7.5
+tax_rate=0.075
+subtotal= quantity * price
+tax=subtotal * tax_rate
+TotalCost= tax + subtotal
+print("Price of the item:",price)
+print("Quantity:",quantity)
+print("Tax rate: %",tax_rate_For_Print)
+
+print("Subtotal:$",subtotal)
+print("Tax:$",tax)
+print("Total:$",TotalCost)
